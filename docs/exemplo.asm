4244de4aa39a26a404ac1e79a5dce6e5cd3402bc
Hugo-NF <hugonfonseca@hotmail.com>
2018-09-30 22:39:15 -0300

    "Project examples and documents"

---
+++ docs/exemplo.asm
@@ -0,0 +1,17 @@
+SECTION TEXT
+ROT: INPUT N1
+SECTION TEXT
+COPY N1, N4 ;comentario qualquer
+ROT2:
+COPY N2, N3
+COPY N3, N3 + 1
+OUTPUT N3 + 1
+STOP
+;comentario qualuer
+;mais um comentario
+SECTION DATA
+N2: CONST -5
+SECTION BSS
+N1: SPACE
+N3: SPACE 2
+N4: SPACE
